8dbd3095e992fec3c73fea3c2b90922895d79f24
Vu Duc Nhuan <nhuanvd@live.com>
2018-12-02 23:17:54 +0700

    update support drawable vector

--- app/src/main/java/com/wallet/crypto/trustapp/App.java
+++ app/src/main/java/com/wallet/crypto/trustapp/App.java
@@ -2,6 +2,7 @@ package com.wallet.crypto.trustapp;
 
 import android.app.Activity;
 import android.support.multidex.MultiDexApplication;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.wallet.crypto.trustapp.di.DaggerAppComponent;
 
@@ -14,23 +15,24 @@ import io.realm.Realm;
 
 public class App extends MultiDexApplication implements HasActivityInjector {
 
-	@Inject
-	DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
         Realm.init(this);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         DaggerAppComponent
-				.builder()
-				.application(this)
-				.build()
-				.inject(this);
-	}
+                .builder()
+                .application(this)
+                .build()
+                .inject(this);
+    }
 
-	@Override
-	public AndroidInjector<Activity> activityInjector() {
-		return dispatchingAndroidInjector;
-	}
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
 
 }
